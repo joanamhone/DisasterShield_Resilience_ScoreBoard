86a5825ccaf9841c261383ba710e2b99ef2c52fc
Ekariorama Magaleta <ekarimagaleta@gmail.com>
2025-07-31 13:40:16 +0200

    edited requirements

--- gee_api.py
+++ gee_api.py
@@ -44,10 +44,7 @@ def get_flood_risk_map():
 
     return jsonify({'map_url': map_url})
 
-#     image = ee.ImageCollection('JRC/GSW1_3/MonthlyHistory') \
-#                 .filterDate('2023-01-01', '2023-12-31') \
-#                 .select('water') \
-#                 .mean() \
-                    
+
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
